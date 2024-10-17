9ebd0c49019b78ebb92a7bdfcef357bb3d73e414
vdsperera <kadvsperera@gmail.com>
2024-10-17 16:03:07 +0530

    fix: resolve wrong method used issue and update comments

--- generators-vs-traditional_method/reading_large_file/generator_method.py
+++ generators-vs-traditional_method/reading_large_file/generator_method.py
@@ -14,7 +14,7 @@ def read_file_generator(p_file_path):
     """
     with open(p_file_path, encoding='utf-8') as file:
         for line in file:
-            yield line
+            yield line.strip()
 
 def process_data(p_file_path):
     """_summary_
@@ -29,11 +29,11 @@ G_FILE_PATH = r"generators-vs-traditional_method/reading_large_file/test_file1.t
 
 # Measure Time and Memory Usage
 g_start_time = time.time()
-mem_usage_traditional  = memory_usage((read_file_generator, (G_FILE_PATH, )))
+mem_usage_traditional  = memory_usage((process_data, (G_FILE_PATH, )))
 g_end_time = time.time()
 
 #Show results
-print(f"Traditional Method: Memory Usage: {max(mem_usage_traditional)} MB")
-print(f"Traditional Method: Execution Time: {g_end_time - g_start_time} seconds")
-# Traditional Method: Memory Usage: 21.87890625 MB
-# Traditional Method: Execution Time: 0.03270864486694336 seconds
+print(f"Generator Method: Memory Usage: {max(mem_usage_traditional)} MB")
+print(f"Generator Method: Execution Time: {g_end_time - g_start_time} seconds")
+# Generator Method: Memory Usage: 21.87890625 MB
+# Generator Method: Execution Time: 0.03270864486694336 seconds
