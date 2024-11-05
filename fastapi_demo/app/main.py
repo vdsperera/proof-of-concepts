5b4ecfc39a22f1b7992fc7b3d7a543b78a9c6940
vdsperera <kadvsperera@gmail.com>
2024-11-05 17:07:54 +0530

    chore: rename item router

--- fastapi_demo/app/main.py
+++ fastapi_demo/app/main.py
@@ -1,11 +1,11 @@
 """_summary_
 """
 from fastapi import FastAPI
-from fastapi_demo.app.routers import items
+from fastapi_demo.app.routers import item_router
 
 app = FastAPI(
     title="FastAPI Demo",
     description="A demonstration of a FastAPI application with professional structure.",
     version="1.0.0",
 )
-app.include_router(router=items.router, tags=["Items"])
+app.include_router(router=item_router.router, tags=["Items"])
