8c780bced900a0395107489887a319c30c51a257
vdsperera <kadvsperera@gmail.com>
2024-11-19 15:17:08 +0530

    xx: update the demonstration of encapsulation in oop and pp

--- oop_vs_functional_programming/encapsulation_vs_function_composition/fp_function_composition.py
+++ oop_vs_functional_programming/encapsulation_vs_function_composition/fp_function_composition.py
@@ -1,8 +1,43 @@
-def describe(name, age):
-    return f"{name} is {age} years old"
+"""_summary_
+"""
+
+def create_account(account_number, balance):
+    """_summary_
+    """
+    print(f"Account: {account_number} is created with balance: {balance}")
+    return {"account_number": account_number, "balance": balance}
 
 
-name = "Vidumini"
-age = 28
+def deposit(account, amount):
+    """_summary_
+    """
+    if amount > 0:
+        account["balance"]  += amount
+        print(f"Deposited {amount} to {account['account_number']}"
+                f"account and new balance is {account['balance']}")
+    else:
+        print(f"Invalid amount: {amount}")
 
-print(describe(name=name, age=age))
+
+def withdraw(account, amount):
+    """_summary_
+    """
+    if amount > 0 and amount < account["balance"]:
+        account["balance"]  -= amount
+        print(f"{amount} is withdrawn from {account['account_number']}"
+                f"and new balance is {account['balance']}")
+    else:
+        print(f"Invalid amount: {amount}")
+
+
+def get_balance(account):
+    """_summary_
+    """
+    print(f"{account['account_number']} account balance is {account['balance']}")
+    return account['balance']
+
+
+account_01 = create_account(account_number="34242342343", balance=25000)
+deposit(account_01, 2000)
+withdraw(account_01, 500)
+get_balance(account_01)
--- oop_vs_functional_programming/encapsulation_vs_function_composition/oop_encapsulation.py
+++ oop_vs_functional_programming/encapsulation_vs_function_composition/oop_encapsulation.py
@@ -1,31 +1,54 @@
 """_summary_
-
-Returns:
-	_type_: _description_
 """
 
+class BankAccount:
+    """
+        summary
+    """
+    def __init__(self, account_number, balance):
+        self.__account_number = account_number
+        self.__balance = balance
+        print(f"Account: {self.__account_number} is created with balance: {balance}")
 
-class Person:
-    """_summary_"""
 
-    def __init__(self, name, age):
+    def deposit(self, amount):
         """_summary_
 
         Args:
-                name (_type_): _description_
-                age (_type_): _description_
+            amount (_type_): _description_
         """
-        self.name = name
-        self.age = age
+        if amount > 0:
+            self.__balance  += amount
+            print(f"Deposited {amount} to {self.__account_number} "
+                  f"account and new balance is {self.__balance}")
+        else:
+            print(f"Invalid amount: {amount}")
 
-    def describe(self):
+
+    def withdraw(self, amount):
+        """_summary_
+
+        Args:
+            amount (_type_): _description_
+        """
+        if amount > 0 and amount < self.__balance:
+            self.__balance  -= amount
+            print(f"{amount} is withdrawn from {self.__account_number} "
+                  f"and new balance is {self.__balance}")
+        else:
+            print(f"Invalid amount: {amount}")
+
+
+    def get_balance(self):
         """_summary_
 
         Returns:
-                _type_: _description_
+            _type_: _description_
         """
-        return f"{self.name} is {self.age} years old"
+        print(f"{self.__account_number} account balance is {self.__balance}")
+        return self.__balance
 
-
-person = Person(name="Vidumini", age=28)
-print(person.describe())
+account_01 = BankAccount(account_number="34242342343", balance=25000)
+account_01.deposit(2000)
+account_01.withdraw(500)
+account_01.get_balance()
